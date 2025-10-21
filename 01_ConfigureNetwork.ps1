f9741f8db7e3c52e3f9a92dea5b57c0a161989e9
SoaB1 <kumachan1230@gmail.com>
2025-10-22 00:32:21 +0900

    ネットワークアダプタの設定を更新し、10GbitおよびGigabitアダプタの静的IP設定機能を削除。デフォルトおよび管理アダプタの設定を改善。

--- 01_ConfigureNetwork.ps1
+++ 01_ConfigureNetwork.ps1
@@ -3,83 +3,126 @@ param (
 )
 
 # Variables
-$TenGigaBitAdpterDesc = "Marvell AQtion 10Gbit Network Adapter"
-$GigaBitAdpterDesc = "Intel(R) Ethernet Controller (3) I225-V"
-
-# Get the network adapter
-$TenGigaBitAdpter = Get-NetAdapter | Where-Object {$_.Status -eq "Up" -and $_.InterfaceDescription -like "*$TenGigaBitAdpterDesc*"}
-$GigaBitAdpter = Get-NetAdapter | Where-Object {$_.Status -eq "Up" -and $_.InterfaceDescription -like "*$GigaBitAdpterDesc*"}
-
 # Read the IP configuration from a file
 if (Test-Path $ConfigFilePath) {
     $Config = Get-Content -Path $ConfigFilePath -Raw | ConvertFrom-Json
-} else {
+}
+else {
     Write-Error "Configuration file not found: $ConfigFilePath"
     exit 1
 }
 
-# Configure the 10Gbit adapter
-if ($null -ne $TenGigaBitAdpter) {
-    # Disable DHCP and set static IP
-    $TenGigaBitAdpter | Set-NetIPInterface -Dhcp Disabled
+$DefaultAdapterSrc = $Config.DefaultLAN.src
+$ManagementAdapterSrc = $Config.ManagementLAN.src
 
-    $NewName = $Config.DefaultLAN.name
-    $IsDefault = $Config.DefaultLAN.default
-    $Ipv4Address = $Config.DefaultLAN.ipv4.address
-    $Ipv4PrefixLength = $Config.DefaultLAN.ipv4.prefixLength
-    $Ipv4DefaultGateway = $Config.DefaultLAN.ipv4.defaultGateway
-    $Ipv4DnsServers = $Config.DefaultLAN.ipv4.dns
+# Get the network adapter
+$DefaultAdapter = Get-NetAdapter | Where-Object { $_.Name -like "*$DefaultAdapterSrc*" }
+$ManagementAdapter = Get-NetAdapter | Where-Object { $_.Name -like "*$ManagementAdapterSrc*" }
 
-    if ($IsDefault -eq $true) {
-        try {
-            $TenGigaBitAdpter | Rename-NetAdapter -NewName $NewName -ErrorAction Stop
-            $TenGigaBitAdpter | New-NetIPAddress -IPAddress $Ipv4Address -PrefixLength $Ipv4PrefixLength -DefaultGateway $Ipv4DefaultGateway -ErrorAction Stop
-            $TenGigaBitAdpter | Set-DnsClientServerAddress -ServerAddresses $Ipv4DnsServers -ErrorAction Stop
-            Write-Host "Configured 10Gbit adapter with static IP: $Ipv4Address"
-        } catch {
-            Write-Error "Failed to configure the 10Gbit adapter: $_"
+# Configure the Default adapter
+$NewName = $Config.DefaultLAN.name
+$IsDefault = $Config.DefaultLAN.default
+$Ipv4Address = $Config.DefaultLAN.ipv4.address
+$Ipv4PrefixLength = $Config.DefaultLAN.ipv4.prefix
+$Ipv4DefaultGateway = $Config.DefaultLAN.ipv4.gateway
+$Ipv4DnsServers = $Config.DefaultLAN.ipv4.dns
+
+if ($IsDefault -eq $true) {
+    try {
+        # Disable DHCP and set static IP
+        $DefaultAdapter | Set-NetIPInterface -Dhcp Disabled > $null
+        $DefaultAdapter | New-NetIPAddress -IPAddress $Ipv4Address -PrefixLength $Ipv4PrefixLength -DefaultGateway $Ipv4DefaultGateway -ErrorAction Stop > $null
+        $DefaultAdapter | Set-DnsClientServerAddress -ServerAddresses $Ipv4DnsServers -ErrorAction Stop > $null
+        if (Get-NetAdapter | Where-Object { $_.Name -eq $NewName }) {
+            Write-Host "Adapter with name $NewName already exists. Skipping rename."
         }
-    } else {
-        try {
-            $TenGigaBitAdpter | Rename-NetAdapter -NewName $NewName -ErrorAction Stop
-            $TenGigaBitAdpter | New-NetIPAddress -IPAddress $Ipv4Address -PrefixLength $Ipv4PrefixLength -ErrorAction Stop
-            $TenGigaBitAdpter | Set-DnsClientServerAddress -ServerAddresses $Ipv4DnsServers -ErrorAction Stop
-            Write-Host "Configured 10Gbit adapter with static IP: $Ipv4Address"
-        } catch {
-            Write-Error "Failed to configure the 10Gbit adapter: $_"
+        else {
+            $DefaultAdapter | Rename-NetAdapter -NewName $NewName -ErrorAction Stop > $null
         }
+        Write-Host "Configured Default adapter with static IP: $Ipv4Address"
+    }
+    catch {
+        Write-Error "Failed to configure the Default adapter: $_"
+    }
+    finally {
+        Start-Sleep -Seconds 30
+        Get-NetIPConfiguration -InterfaceAlias $NewName
+    }
+}
+else {
+    try {
+        $DefaultAdapter | New-NetIPAddress -IPAddress $Ipv4Address -PrefixLength $Ipv4PrefixLength -ErrorAction Stop > $null
+        $DefaultAdapter | Set-DnsClientServerAddress -ServerAddresses $Ipv4DnsServers -ErrorAction Stop > $null
+        if (Get-NetAdapter | Where-Object { $_.Name -eq $NewName }) {
+            Write-Host "Adapter with name $NewName already exists. Skipping rename."
+        }
+        else {
+            $DefaultAdapter | Rename-NetAdapter -NewName $NewName -ErrorAction Stop > $null
+        }
+        Write-Host "Configured Default adapter with static IP: $Ipv4Address"
+    }
+    catch {
+        Write-Error "Failed to configure the Default adapter: $_"
+    }
+    finally {
+        Start-Sleep -Seconds 30
+        Get-NetIPConfiguration -InterfaceAlias $NewName
     }
 }
 
-# Configure the Gigabit adapter
-if ($null -ne $GigaBitAdpter) {
-    # Disable DHCP and set static IP
-    $GigaBitAdpter | Set-NetIPInterface -Dhcp Disabled
-
+# Configure the Management adapter
+if ($null -ne $ManagementAdapter) {
     $NewName = $Config.ManagementLAN.name
     $IsDefault = $Config.ManagementLAN.default
     $Ipv4Address = $Config.ManagementLAN.ipv4.address
-    $Ipv4PrefixLength = $Config.ManagementLAN.ipv4.prefixLength
-    $Ipv4DefaultGateway = $Config.ManagementLAN.ipv4.defaultGateway
+    $Ipv4PrefixLength = $Config.ManagementLAN.ipv4.prefix
+    $Ipv4DefaultGateway = $Config.ManagementLAN.ipv4.gateway
     $Ipv4DnsServers = $Config.ManagementLAN.ipv4.dns
 
     if ($IsDefault -eq $true) {
         try {
-            $GigaBitAdpter | Rename-NetAdapter -NewName $NewName -ErrorAction Stop
-            $GigaBitAdpter | New-NetIPAddress -IPAddress $Ipv4Address -PrefixLength $Ipv4PrefixLength -DefaultGateway $Ipv4DefaultGateway -ErrorAction Stop
-            $GigaBitAdpter | Set-DnsClientServerAddress -ServerAddresses $Ipv4DnsServers -ErrorAction Stop
-            Write-Host "Configured Gigabit adapter with static IP: $Ipv4Address"
-        } catch {
-            Write-Error "Failed to configure the Gigabit adapter: $_"
+            # Disable DHCP and set static IP
+            $ManagementAdapter | Set-NetIPInterface -Dhcp Disabled > $null
+            $ManagementAdapter | New-NetIPAddress -IPAddress $Ipv4Address -PrefixLength $Ipv4PrefixLength -DefaultGateway $Ipv4DefaultGateway -ErrorAction Stop > $null
+            $ManagementAdapter | Set-DnsClientServerAddress -ServerAddresses $Ipv4DnsServers -ErrorAction Stop > $null
+            if (Get-NetAdapter | Where-Object { $_.Name -eq $NewName }) {
+                Write-Host "Adapter with name $NewName already exists. Skipping rename."
+            }
+            else {
+                $ManagementAdapter | Rename-NetAdapter -NewName $NewName -ErrorAction Stop > $null
+            }
+            Write-Host "Configured Management adapter with static IP: $Ipv4Address"
         }
-    } else {
+        catch {
+            Write-Error "Failed to configure the Management adapter: $_"
+        }
+        finally {
+            Start-Sleep -Seconds 30
+            Get-NetIPConfiguration -InterfaceAlias $NewName
+        }
+    }
+    else {
         try {
-            $GigaBitAdpter | Rename-NetAdapter -NewName $NewName -ErrorAction Stop
-            $GigaBitAdpter | New-NetIPAddress -IPAddress $Ipv4Address -PrefixLength $Ipv4PrefixLength -ErrorAction Stop
-            $GigaBitAdpter | Set-DnsClientServerAddress -ServerAddresses $Ipv4DnsServers -ErrorAction Stop
-            Write-Host "Configured Gigabit adapter with static IP: $Ipv4Address"
-        } catch {
-            Write-Error "Failed to configure the Gigabit adapter: $_"
+            # Disable DHCP and set static IP
+            $ManagementAdapter | Set-NetIPInterface -Dhcp Disabled > $null
+            $ManagementAdapter | New-NetIPAddress -IPAddress $Ipv4Address -PrefixLength $Ipv4PrefixLength -ErrorAction Stop > $null
+            $ManagementAdapter | Set-DnsClientServerAddress -ServerAddresses $Ipv4DnsServers -ErrorAction Stop > $null
+            if (Get-NetAdapter | Where-Object { $_.Name -eq $NewName }) {
+                Write-Host "Adapter with name $NewName already exists. Skipping rename."
+            }
+            else {
+                $ManagementAdapter | Rename-NetAdapter -NewName $NewName -ErrorAction Stop > $null
+            }
+            Write-Host "Configured Management adapter with static IP: $Ipv4Address"
+        }
+        catch {
+            Write-Error "Failed to configure the Management adapter: $_"
+        }
+        finally {
+            Start-Sleep -Seconds 30
+            Get-NetIPConfiguration -InterfaceAlias $NewName
         }
     }
 }
+
+Write-Host "Network configuration completed."
